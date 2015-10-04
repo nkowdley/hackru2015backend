07a77e5d1f510ebb0e28fd91aad08b44e97df779
Neel Kowdley <nkowdley@gmail.com>
2015-10-04 09:26:37 -0400

    Updated Front End

--- routes/joinAct.js
+++ routes/joinAct.js
@@ -10,24 +10,30 @@ router.post('/', function(req, res, next) {
   //get the info we need to update the databases
   var user=req.body.name;
   var activity1=req.body.id;
+  var activity2=activity1.toString();
+  //var idString="ObjectId("+activity1+")";
   var query = {'_id':activity};
   console.log(user);
-  console.log(activity1);
+  console.log("act: "+activity);
   console.log("Yolo1");
   //update the activity to show number of people going
   db.collection('new').findOneAndUpdate(query, {$inc : {'numberofPeople' : 1}}, {upsert:false}, function(err, doc){
+    console.log("success1");
     if (err)
     {
       return res.send(500, { error: err });
     }
+    console.log("sucess!");
   });
   //update the events a user is attending
   var query = {'username':user};
+  console.log("yolo2");
   db.collection('users').findOneAndUpdate(query, {$push: {activities: activity1}}, {upsert:true}, function(err, doc){
     if (err)
     {
       return res.send(500, { error: err });
     }
   });
+  res.send("ok");
 });
 module.exports = router;
